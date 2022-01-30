121b96372ffdd5219d117f6365db1c0e132c9ccc
Luis Jacobetty <luisjs92@gmail.com>
2022-01-30 21:08:12 +0000

    Add Expr AST classes (chapters 5.2 and 5.3)

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@
 import { runFile, runPrompt } from "./Lox";
 
 const args = process.argv.slice(2);
-
 if (args.length > 1) {
     console.error("Usage: tslox [path-to-script]");
+    process.exit(64);
 }
 else if (args.length == 1) {
     runFile(args[0]);
